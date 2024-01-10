6f7b94b850f904643f1f8bf92c3f9ed611e57de8
rishabh11336 <rishabh11336@gmail.com>
2024-01-10 14:10:03 +0530

    inital setup mad2project from complete start

---
+++ BackEnd/app.py
@@ -0,0 +1,21 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+from .config.config import Config
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
