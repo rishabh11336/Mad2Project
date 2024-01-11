c8a9cd4e58cd085c4d9a58ddd6a36a0f11346fda
rishabh11336 <rishabh11336@gmail.com>
2024-01-11 15:53:33 +0530

    jwt added token working on project still need to figure token blaclist part

--- BackEnd/app/config/config.py
+++ BackEnd/app/config/config.py
@@ -1,7 +1,14 @@
 import os
 
+from datetime import timedelta
+
 class Config:
     Upload_Folder = os.path.join(os.getcwd(), '..', 'static')
     
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    JWT_SECRET_KEY = os.urandom(24)
+    JWT_BLACKLIST_ENABLED = True
+    JWT_BLACKLIST_TOKEN_CHECKS = ["access", "refresh"]
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(seconds=1000000)
